59eb62c9a3f9dcc075d5ee6d552c92419c2721e2
Joris Clement <j.clement@campus.tu-berlin.de>
2020-10-02 18:08:19 +0200

    Improve wording

--- tests/test_visualize.py
+++ tests/test_visualize.py
@@ -3,7 +3,7 @@ import click.testing
 from thesis import visualize
 
 
-def test_visualize_succeeds():
+def test_visualize_main_succeeds():
     runner = click.testing.CliRunner()
     result = runner.invoke(visualize.main, ["./testdata/small_measurement_excerpt.csv"])
     assert result.exit_code == 0
