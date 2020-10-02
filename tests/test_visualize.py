9583264842ae78caaf2899bd9a6ee75a60c10191
Joris Clement <j.clement@campus.tu-berlin.de>
2020-10-02 13:55:16 +0200

    Wording

--- tests/test_visualize.py
+++ tests/test_visualize.py
@@ -3,7 +3,7 @@ import click.testing
 from thesis import visualize
 
 
-def test_main_succeeds():
+def test_visualize_succeeds():
     runner = click.testing.CliRunner()
     result = runner.invoke(visualize.main, ["./testdata/small_measurement_excerpt.csv"])
     assert result.exit_code == 0
