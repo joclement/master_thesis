3f9b49542bc5d09280574bd61b93e05bff5a5448
Joris Clement <j.clement@campus.tu-berlin.de>
2020-10-02 13:10:42 +0200

    Initial commit
    - Add printing of csv file as a dummy start
    - Setup implementation using poetry

---
+++ src/thesis/visualize.py
@@ -0,0 +1,14 @@
+import pandas as pd
+import click
+
+from . import __version__
+
+
+@click.command()
+@click.version_option(version=__version__)
+@click.argument('measurement_filepath', type=click.Path(exists=True))
+def main(measurement_filepath):
+    "Plot visualization of measurement file csv"
+
+    df = pd.read_csv(measurement_filepath, sep=";")
+    click.echo(df.head(10))
