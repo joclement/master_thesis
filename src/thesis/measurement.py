8323e31fae0e9518f5b9db78d2252966f64946db
Joris Clement <j.clement@campus.tu-berlin.de>
2020-10-05 10:58:14 +0200

    Add 2 plots to visualize measurement file, #2
    The plots are:
    - PDs over time
    - Time difference between PDs over time
    
    The intention is to get a better understanding of the data by
    replicating the plots.

--- src/thesis/measurement.py
+++ src/thesis/measurement.py
@@ -1,10 +1,13 @@
 import pandas as pd
 
+TIMEDIFF = "TimeDiff [s]"
+PD = "A [nV]"
+TEST_VOLTAGE = "Voltage [kV]"
+TIME = "Time [s]"
+
 
 def read(filepath):
     df = pd.read_csv(filepath, sep=";", decimal=",")
-    assert df["Time [s]"][0] == 0
-    df["Time [s]"] = pd.to_timedelta(df["Time [s]"], "seconds")
-    df["TimeDiff [s]"] = df["Time [s]"].diff()
-    df = df.drop(columns=["Time [s]"])
+    assert df[TIME][0] == 0
+    df[TIMEDIFF] = df[TIME].diff()
     return df
--- src/thesis/visualize.py
+++ src/thesis/visualize.py
@@ -1,4 +1,5 @@
 import click
+import matplotlib.pyplot as plt
 
 from . import __version__, measurement
 
@@ -6,8 +7,25 @@ from . import __version__, measurement
 @click.command()
 @click.version_option(version=__version__)
 @click.argument("measurement_filepath", type=click.Path(exists=True))
-def main(measurement_filepath):
+@click.option("-o", "--output-folder", type=click.Path(exists=True))
+@click.option("--show/--no-show", default=False)
+def main(measurement_filepath, output_folder, show):
     "Plot visualization of measurement file csv"
 
     df = measurement.read(measurement_filepath)
-    click.echo(df.head(10))
+
+    plt.scatter(df[measurement.TIME], df[measurement.PD], marker=".")
+    plt.xlabel("t in seconds")
+    plt.ylabel("PD in nV")
+    if output_folder:
+        plt.savefig(f"{output_folder}/PDOverTime.png")
+    if show:
+        plt.show()
+
+    plt.scatter(df[measurement.TIME][1:], df[measurement.TIMEDIFF][1:], marker=".")
+    plt.xlabel("t in seconds")
+    plt.ylabel("Δt in seconds")
+    if output_folder:
+        plt.savefig(f"{output_folder}/DeltaTOverTime.png")
+    if show:
+        plt.show()
