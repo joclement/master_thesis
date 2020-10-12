4a63f6ae8a2c87314fbb8d506d2b761833c42c8a
Joris Clement <j.clement@campus.tu-berlin.de>
2020-10-12 22:58:16 +0200

    Improve nox sessions by limiting installs
    This idea is from here:
    https://cjolowicz.github.io/posts/hypermodern-python-03-linting/

--- noxfile.py
+++ noxfile.py
@@ -1,3 +1,5 @@
+import tempfile
+
 import nox
 
 nox.options.sessions = "lint", "mypy", "tests"
@@ -5,28 +7,43 @@ nox.options.sessions = "lint", "mypy", "tests"
 LOCATIONS = "src", "tests", "noxfile.py"
 
 
+def install_with_constraints(session, *args, **kwargs):
+    with tempfile.NamedTemporaryFile() as requirements:
+        session.run(
+            "poetry",
+            "export",
+            "--dev",
+            "--format=requirements.txt",
+            f"--output={requirements.name}",
+            external=True,
+        )
+        session.install(f"--constraint={requirements.name}", *args, **kwargs)
+
+
 @nox.session(python=["3.8"])
 def tests(session):
-    session.run("poetry", "install", external=True)
-    session.run("pytest", "--cov")
+    args = session.posargs or ["--cov"]
+    session.run("poetry", "install", "--no-dev", external=True)
+    install_with_constraints(session, "coverage[toml]", "pytest", "pytest-cov")
+    session.run("pytest", *args)
 
 
 @nox.session(python=["3.8"])
 def lint(session):
     args = session.posargs or LOCATIONS
-    session.install("flake8", "flake8-black", "flake8-import-order")
+    install_with_constraints(session, "flake8", "flake8-black", "flake8-import-order")
     session.run("flake8", *args)
 
 
 @nox.session(python="3.8")
 def black(session):
     args = session.posargs or LOCATIONS
-    session.install("black")
+    install_with_constraints(session, "black")
     session.run("black", *args)
 
 
 @nox.session(python="3.8")
 def mypy(session):
     args = session.posargs or LOCATIONS
-    session.install("mypy")
+    install_with_constraints(session, "mypy")
     session.run("mypy", *args)
