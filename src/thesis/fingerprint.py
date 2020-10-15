32339403a0ed19def09807aadf3dd1fc92444e2b
Joris Clement <j.clement@campus.tu-berlin.de>
2020-10-15 22:22:38 +0200

    Add fingerprint from TU Graz research group
    This can be used further as input for classifiers.
    
    Fingerprint described in this paper:
    B. Schober and U. Schichler, “Application of Machine Learning for
    Partial Discharge Classification under DC Voltage,” in Proceedings of
    the Nordic Insulation Symposium, 2019, no. 26, pp. 16–21.
    
    Close #21

---
+++ src/thesis/fingerprint.py
@@ -0,0 +1,50 @@
+from typing import Tuple, Union
+
+import pandas as pd
+import scipy.stats as stats
+
+from . import data
+
+PD_VAR = "PD Variance"
+PD_SKEW = "PD Skewness"
+PD_KURT = "PD Kurtosis"
+PD_WEIBULL_A = "PD Weibull A"
+PD_WEIBULL_B = "PD Weibull B"
+TD_MAX = "TimeDiff Max"
+TD_MEAN = "TimeDiff Mean"
+TD_MIN = "TimeDiff Min"
+TD_VAR = "TimeDiff Variance"
+TD_SKEW = "TimeDiff Skewness"
+TD_KURT = "TimeDiff Kurtosis"
+TD_WEIBULL_A = "TimeDiff Weibull A"
+TD_WEIBULL_B = "TimeDiff Weibull B"
+PDS_PER_SEC = "Number of PDs/sec"
+
+
+# TODO ensure that weibull fit is correct
+def calc_weibull_params(data: Union[list, pd.Series]) -> Tuple[float, float]:
+    weibull_a, _, weibull_b = stats.weibull_min.fit(data, floc=0.0)
+    return weibull_a, weibull_b
+
+
+def tu_graz(df: pd.DataFrame) -> pd.Series:
+    finger = pd.Series(dtype=float)
+
+    finger[PD_VAR] = df[data.PD].var()
+    # TODO ensure that skewness is not 0 because of numerical problem
+    finger[PD_SKEW] = df[data.PD].skew()
+    # TODO ensure that skewness is not 0 because of numerical problem
+    finger[PD_KURT] = df[data.PD].kurt()
+    finger[PD_WEIBULL_A], finger[PD_WEIBULL_B] = calc_weibull_params(df[data.PD])
+    finger[TD_MAX] = df[data.TIMEDIFF].max()
+    finger[TD_MEAN] = df[data.TIMEDIFF].mean()
+    finger[TD_MIN] = df[data.TIMEDIFF].min()
+    finger[TD_VAR] = df[data.TIMEDIFF].var()
+    finger[TD_SKEW] = df[data.TIMEDIFF].skew()
+    finger[TD_KURT] = df[data.TIMEDIFF].kurt()
+    finger[TD_WEIBULL_A], finger[TD_WEIBULL_B] = calc_weibull_params(
+        df[data.TIMEDIFF][1:]
+    )
+    finger[PDS_PER_SEC] = len(df[data.TIME]) / df[data.TIME].max()
+
+    return finger
