8f6e550d93dcd8729c6f7bb21e49d78038dd44e5
Joris Clement <j.clement@campus.tu-berlin.de>
2020-10-19 22:48:56 +0200

    Style: Short variable name to keep lines readable

--- src/thesis/fingerprint.py
+++ src/thesis/fingerprint.py
@@ -8,16 +8,16 @@ from . import data
 PD_VAR = "PD Variance"
 PD_SKEW = "PD Skewness"
 PD_KURT = "PD Kurtosis"
-PD_WEIBULL_A = "PD Weibull A"
-PD_WEIBULL_B = "PD Weibull B"
+PD_WEIB_A = "PD Weibull A"
+PD_WEIB_B = "PD Weibull B"
 TD_MAX = "TimeDiff Max"
 TD_MEAN = "TimeDiff Mean"
 TD_MIN = "TimeDiff Min"
 TD_VAR = "TimeDiff Variance"
 TD_SKEW = "TimeDiff Skewness"
 TD_KURT = "TimeDiff Kurtosis"
-TD_WEIBULL_A = "TimeDiff Weibull A"
-TD_WEIBULL_B = "TimeDiff Weibull B"
+TD_WEIB_A = "TimeDiff Weibull A"
+TD_WEIB_B = "TimeDiff Weibull B"
 PDS_PER_SEC = "Number of PDs/sec"
 
 
@@ -35,14 +35,15 @@ def tu_graz(df: pd.DataFrame) -> pd.Series:
     finger[PD_SKEW] = df[data.PD].skew()
     # TODO Issue #23: ensure that skewness is not 0 because of numerical problem
     finger[PD_KURT] = df[data.PD].kurt()
-    finger[PD_WEIBULL_A], finger[PD_WEIBULL_B] = calc_weibull_params(df[data.PD])
+    finger[PD_WEIB_A], finger[PD_WEIB_B] = calc_weibull_params(df[data.PD])
+
     finger[TD_MAX] = df[data.TIMEDIFF].max()
     finger[TD_MEAN] = df[data.TIMEDIFF].mean()
     finger[TD_MIN] = df[data.TIMEDIFF].min()
     finger[TD_VAR] = df[data.TIMEDIFF].var()
     finger[TD_SKEW] = df[data.TIMEDIFF].skew()
     finger[TD_KURT] = df[data.TIMEDIFF].kurt()
-    finger[TD_WEIBULL_A], finger[TD_WEIBULL_B] = calc_weibull_params(
+    finger[TD_WEIB_A], finger[TD_WEIB_B] = calc_weibull_params(
         df[data.TIMEDIFF][1:]
     )
     finger[PDS_PER_SEC] = len(df[data.TIME]) / df[data.TIME].max()
