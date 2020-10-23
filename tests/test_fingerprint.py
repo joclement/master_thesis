02ad6d19a207d1b7c988b1fef422cb0bce0622f4
Joris Clement <j.clement@campus.tu-berlin.de>
2020-10-23 15:09:52 +0200

    Test that fingerprint contains only valid floats

--- tests/test_fingerprint.py
+++ tests/test_fingerprint.py
@@ -1,3 +1,5 @@
+import math
+
 import pandas as pd
 
 from thesis import fingerprint
@@ -7,6 +9,8 @@ def test_fingerprint_tu_graz(measurement):
     finger = fingerprint.tu_graz(measurement)
     assert type(finger) is pd.Series
     assert len(finger) == 14
+    assert all([not math.isnan(value) for value in finger])
+    assert finger.dtype == "float64"
     assert len(finger) == len(set(finger.tolist()))
 
 
