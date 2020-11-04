59c2f7243916a67e8506d63dd2a9c450c7ac1cb0
Joris Clement <j.clement@campus.tu-berlin.de>
2020-11-04 13:51:03 +0100

    Improve naming for generated bar plot
    Use more appropriate names.

--- tests/test_classify.py
+++ tests/test_classify.py
@@ -24,38 +24,28 @@ def test_classify_main_succeeds(multiple_csv_files, tmpdir):
     result = runner.invoke(classify.main, [multiple_csv_files, str(tmpdir)])
     assert result.exit_code == 0
     ones = np.ones(4)
-    assert (
-        f"Accuracies for LukasMeanDist with fingerprint TU Graz: {ones}"
-        in result.output
-    )
-    assert (
-        f"Accuracies for LukasMeanDist with fingerprint Lukas: {ones}" in result.output
-    )
-    assert (
-        f"Accuracies for LukasMeanDist with fingerprint Lukas + TU Graz: {ones}"
-        in result.output
-    )
+    assert f"Accuracies for Ott with fingerprint TU Graz: {ones}" in result.output
+    assert f"Accuracies for Ott with fingerprint Ott: {ones}" in result.output
+    assert f"Accuracies for Ott with fingerprint Ott + TU Graz: {ones}" in result.output
 
     assert f"Accuracies for 1-NN with fingerprint TU Graz: {ones}" in result.output
-    assert f"Accuracies for 3-NN with fingerprint Lukas: {ones}" in result.output
+    assert f"Accuracies for 3-NN with fingerprint Ott: {ones}" in result.output
     assert (
-        f"Accuracies for 3-NN with fingerprint Lukas + TU Graz: {ones}" in result.output
+        f"Accuracies for 3-NN with fingerprint Ott + TU Graz: {ones}" in result.output
     )
 
     assert f"Accuracies for SVM with fingerprint TU Graz: {ones}" in result.output
-    assert f"Accuracies for SVM with fingerprint Lukas: {ones}" in result.output
-    assert (
-        f"Accuracies for SVM with fingerprint Lukas + TU Graz: {ones}" in result.output
-    )
+    assert f"Accuracies for SVM with fingerprint Ott: {ones}" in result.output
+    assert f"Accuracies for SVM with fingerprint Ott + TU Graz: {ones}" in result.output
 
     assert "Accuracies for MLP with fingerprint TU Graz: " in result.output
-    assert "Accuracies for MLP with fingerprint Lukas:" in result.output
-    assert "Accuracies for MLP with fingerprint Lukas + TU Graz: " in result.output
+    assert "Accuracies for MLP with fingerprint Ott:" in result.output
+    assert "Accuracies for MLP with fingerprint Ott + TU Graz: " in result.output
 
     assert result.output.count("Confusion matrix") == 15
 
     assert Path(tmpdir, "bar.svg").exists()
-    assert Path(tmpdir, "confusion_matrix_SVM_Lukas.svg").exists()
+    assert Path(tmpdir, "confusion_matrix_SVM_Ott.svg").exists()
     assert len(list(Path(tmpdir).rglob("confusion_matrix_*.svg"))) == 15
 
 
