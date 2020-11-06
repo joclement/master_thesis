0f95fdfff6576c848a36cdfdd1f535d2cfeac123
Joris Clement <j.clement@campus.tu-berlin.de>
2020-11-06 11:53:25 +0100

    Improve name for visualize PD csv files cli tool

---
+++ tests/test_visualize_pd_data.py
@@ -0,0 +1,30 @@
+import click.testing
+
+from thesis import visualize_pd_data
+
+
+def test_visualize_pd_data_main_succeeds(csv_filepath):
+    runner = click.testing.CliRunner()
+    result = runner.invoke(visualize_pd_data.main, ["--no-show", csv_filepath])
+    assert result.exit_code == 0
+
+
+def test_visualize_pd_data_main_save_succeeds(csv_filepath):
+    runner = click.testing.CliRunner()
+    result = runner.invoke(
+        visualize_pd_data.main,
+        ["--output-folder", "/tmp/", csv_filepath],
+    )
+    assert result.exit_code == 0
+
+
+def test_visualize_pd_data_version_succeeds():
+    runner = click.testing.CliRunner()
+    result = runner.invoke(visualize_pd_data.main, ["--version"])
+    assert result.exit_code == 0
+
+
+def test_visualize_pd_data_help_succeeds():
+    runner = click.testing.CliRunner()
+    result = runner.invoke(visualize_pd_data.main, ["--help"])
+    assert result.exit_code == 0
