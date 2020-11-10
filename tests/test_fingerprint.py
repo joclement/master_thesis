Fix scale of PD data to avoid numerical problems
The skewness and curtosis are no longer 0 due to a better scale.

Close #23

tests/test_fingerprint.py
@@ -65,5 +65,6 @@ def test_normalize_fingerprints(measurements):
     scaler.fit(fingerprints)
     normalized_fingers = scaler.transform(fingerprints)
 
-    assert all(np.amax(normalized_fingers, axis=0) <= 1)
+    for max_value in np.amax(normalized_fingers, axis=0):
+        assert math.isclose(max_value, 1.0) or max_value == 0.0
     assert all(np.amin(normalized_fingers, axis=0) == 0)