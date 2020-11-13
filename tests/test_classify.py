f4ed580538e04ecc067f1a3f24f47c36c3327c20
Joris Clement <j.clement@campus.tu-berlin.de>
2020-11-13 19:36:49 +0100

    Measure classifier's score with balanced accuracy
    This might be a more useful metric, because the number of samples per
    defect is unbalanced.

--- tests/test_classify.py
+++ tests/test_classify.py
@@ -24,30 +24,28 @@ def test_classify_main_succeeds(multiple_csv_files, tmpdir):
     result = runner.invoke(classify.main, [multiple_csv_files, str(tmpdir)])
     assert result.exit_code == 0
     ones = np.ones(4)
-    assert f"Accuracies for Ott with fingerprint TU Graz: {ones}" in result.output
-    assert f"Accuracies for Ott with fingerprint Ott: {ones}" in result.output
-    assert f"Accuracies for Ott with fingerprint Ott + TU Graz: {ones}" in result.output
+    assert f"Scores for Ott with fingerprint TU Graz: {ones}" in result.output
+    assert f"Scores for Ott with fingerprint Ott: {ones}" in result.output
+    assert f"Scores for Ott with fingerprint Ott + TU Graz: {ones}" in result.output
 
-    assert f"Accuracies for 1-NN with fingerprint TU Graz: {ones}" in result.output
-    assert f"Accuracies for 3-NN with fingerprint Ott: {ones}" in result.output
-    assert (
-        f"Accuracies for 3-NN with fingerprint Ott + TU Graz: {ones}" in result.output
-    )
+    assert f"Scores for 1-NN with fingerprint TU Graz: {ones}" in result.output
+    assert f"Scores for 3-NN with fingerprint Ott: {ones}" in result.output
+    assert f"Scores for 3-NN with fingerprint Ott + TU Graz: {ones}" in result.output
 
-    assert f"Accuracies for SVM with fingerprint TU Graz: {ones}" in result.output
-    assert f"Accuracies for SVM with fingerprint Ott: {ones}" in result.output
-    assert f"Accuracies for SVM with fingerprint Ott + TU Graz: {ones}" in result.output
+    assert f"Scores for SVM with fingerprint TU Graz: {ones}" in result.output
+    assert f"Scores for SVM with fingerprint Ott: {ones}" in result.output
+    assert f"Scores for SVM with fingerprint Ott + TU Graz: {ones}" in result.output
 
-    assert "Accuracies for MLP with fingerprint TU Graz: " in result.output
-    assert "Accuracies for MLP with fingerprint Ott:" in result.output
-    assert "Accuracies for MLP with fingerprint Ott + TU Graz: " in result.output
+    assert "Scores for MLP with fingerprint TU Graz: " in result.output
+    assert "Scores for MLP with fingerprint Ott:" in result.output
+    assert "Scores for MLP with fingerprint Ott + TU Graz: " in result.output
 
-    assert "Accuracies for 1-NN DTW with Time Series: " in result.output
-    assert "Accuracies for 3-NN DTW with Time Series: " in result.output
+    assert "Scores for 1-NN DTW with Time Series: " in result.output
+    assert "Scores for 3-NN DTW with Time Series: " in result.output
 
     assert result.output.count("Confusion matrix") == 17
 
-    assert Path(tmpdir, "classifiers_accuracy_bar.svg").exists()
+    assert Path(tmpdir, "classifiers_balanced_accuracy_bar.svg").exists()
     assert Path(tmpdir, "confusion_matrix_SVM_fingerprint_Ott.svg").exists()
     assert len(list(Path(tmpdir).rglob("confusion_matrix_*.svg"))) == 17
 
