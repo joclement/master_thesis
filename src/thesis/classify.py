69e64c739c86cce6c52c03bd39dee8197535f763
Joris Clement <j.clement@campus.tu-berlin.de>
2020-12-09 19:00:13 +0100

    Improve plot, reduce global variables, cleanup
    Improve the plot by using the same classifier name for equivalent
    classifiers on time series and fingerprint classification.
    
    Construct code more object-oriented and thereby reduce global variables.

--- src/thesis/classify.py
+++ src/thesis/classify.py
@@ -15,49 +15,6 @@ from tslearn.utils import to_time_series_dataset
 
 from . import __version__, classifiers, data, fingerprint, util
 
-FINGERPRINTS = {
-    "Ott": fingerprint.lukas,
-    "TU Graz": fingerprint.tu_graz,
-    "Ott + TU Graz": fingerprint.lukas_plus_tu_graz,
-}
-TS = "TS"
-ONED_TS = f"1D {TS}"
-TWOD_TS = f"2D {TS}"
-FINGER_SEQUENCES = {f"{finger} Seq": algo for finger, algo in FINGERPRINTS.items()}
-DATASET_NAMES = list(FINGERPRINTS.keys()) + [ONED_TS, TWOD_TS, *FINGER_SEQUENCES]
-
-FINGERPRINT_CLASSIFIERS = {
-    "1-NN": KNeighborsClassifier(n_neighbors=1),
-    "3-NN": KNeighborsClassifier(n_neighbors=3),
-    "Ott Algo": classifiers.LukasMeanDist(),
-    "SVM": svm.SVC(decision_function_shape="ovo"),
-    "MLP": neural_network.MLPClassifier(
-        hidden_layer_sizes=(9,),
-        solver="lbfgs",
-    ),
-    "DT": DecisionTreeClassifier(),
-}
-
-SEQUENCE_CLASSIFIERS = {
-    "1-NN DTW": KNeighborsTimeSeriesClassifier(n_neighbors=1),
-    "3-NN DTW": KNeighborsTimeSeriesClassifier(n_neighbors=3),
-}
-
-CLASSIFIERS = {
-    **FINGERPRINT_CLASSIFIERS,
-    **SEQUENCE_CLASSIFIERS,
-}
-
-CV = 4
-SCORE_METRIC = "balanced_accuracy"
-SCORE_METRIC_NAME = SCORE_METRIC.replace("_", " ")
-
-MAX_FREQUENCY = pd.tseries.frequencies.to_offset("50us")
-FREQUENCY = pd.tseries.frequencies.to_offset("1s")
-FINGERPRINT_SEQUENCE_DURATION = pd.Timedelta("30 seconds")
-
-N_JOBS = -2
-
 
 def _echo_visual_break():
     click.echo()
@@ -105,59 +62,58 @@ def _get_defect_names(measurements: List[pd.DataFrame]):
     ]
 
 
-def _convert_to_time_series(df: pd.DataFrame) -> pd.Series:
-    df[data.TIME] = pd.to_datetime(df[data.TIME], unit=data.TIME_UNIT)
-    df.set_index(data.TIME, inplace=True)
-    time_series = df[data.PD]
-    return time_series.asfreq(MAX_FREQUENCY, fill_value=0.0).resample(FREQUENCY).max()
-
-
-def _build_fingerprint_sequence(df: pd.DataFrame, finger_algo):
-    timedelta_sum = pd.Timedelta(0)
-    index_sequence_splits = []
-    for index, value in df[data.TIME_DIFF][1:].iteritems():
-        timedelta_sum += pd.Timedelta(value, unit=data.TIME_UNIT)
-        if timedelta_sum >= FINGERPRINT_SEQUENCE_DURATION:
-            index_sequence_splits.append(index)
-            timedelta_sum = pd.Timedelta(0)
-    sequence = [df.iloc[: index_sequence_splits[0]]]
-    for idx in range(1, len(index_sequence_splits)):
-        sequence.append(
-            df.iloc[index_sequence_splits[idx - 1] : index_sequence_splits[idx]]
-        )
-
-    too_short_indexes = []
-    for index, sub_df in enumerate(sequence):
-        if len(sub_df.index) <= 2:
-            too_short_indexes.append(index)
-
-    if len(too_short_indexes) > 0:
-        if (
-            len(too_short_indexes) - len(range(too_short_indexes[0], len(sequence)))
-            <= 1
-        ):
-            del sequence[too_short_indexes[0] :]
-        else:
-            raise ValueError(
-                f"Invalid measurement file: {FINGERPRINT_SEQUENCE_DURATION}"
-            )
-
-    assert all([len(sub_df.index) >= 3 for sub_df in sequence])
-    assert all([not sub_df.index.isnull().any() for sub_df in sequence])
-    assert len(sequence) >= 3
-
-    return fingerprint.build_set(sequence, finger_algo, False).to_numpy()
-
-
 class ClassificationHandler:
     def __init__(
         self, measurements: List[pd.DataFrame], output_directory, calc_cm: bool
     ):
+        self.FINGERPRINTS = {
+            "Ott": fingerprint.lukas,
+            "TU Graz": fingerprint.tu_graz,
+            "Ott + TU Graz": fingerprint.lukas_plus_tu_graz,
+        }
+        TS = "TS"
+        self.ONED_TS = f"1D {TS}"
+        self.TWOD_TS = f"2D {TS}"
+        self.FINGER_SEQUENCES = {
+            f"{finger} Seq": algo for finger, algo in self.FINGERPRINTS.items()
+        }
+
+        self.FINGERPRINT_CLASSIFIERS = {
+            ("1-NN", KNeighborsClassifier(n_neighbors=1)),
+            ("3-NN", KNeighborsClassifier(n_neighbors=3)),
+            ("Ott Algo", classifiers.LukasMeanDist()),
+            ("SVM", svm.SVC(decision_function_shape="ovo")),
+            (
+                "MLP",
+                neural_network.MLPClassifier(hidden_layer_sizes=(9,), solver="lbfgs"),
+            ),
+            ("DT", DecisionTreeClassifier()),
+        }
+        self.SEQUENCE_CLASSIFIERS = {
+            ("1-NN", KNeighborsTimeSeriesClassifier(n_neighbors=1)),
+            ("3-NN", KNeighborsTimeSeriesClassifier(n_neighbors=3)),
+        }
+
+        DATASET_NAMES = list(self.FINGERPRINTS.keys()) + [
+            self.ONED_TS,
+            self.TWOD_TS,
+            *self.FINGER_SEQUENCES,
+        ]
+        CLASSIFIERS = self.FINGERPRINT_CLASSIFIERS | self.SEQUENCE_CLASSIFIERS
         self.mean_accuracies = pd.DataFrame(
-            {c: np.empty(len(DATASET_NAMES)) for c in CLASSIFIERS.keys()},
+            {c[0]: np.empty(len(DATASET_NAMES)) for c in CLASSIFIERS},
             index=[c for c in DATASET_NAMES],
         )
         self.std_accuracies = self.mean_accuracies.copy(deep=True)
+
+        self.CV = 4
+        self.SCORE_METRIC = "balanced_accuracy"
+
+        self.MAX_FREQUENCY = pd.tseries.frequencies.to_offset("50us")
+        self.FREQUENCY = pd.tseries.frequencies.to_offset("1s")
+        self.FINGERPRINT_SEQUENCE_DURATION = pd.Timedelta("30 seconds")
+
+        self.N_JOBS = -2
 
         self.measurements = measurements
         self.calc_cm = calc_cm
@@ -173,10 +129,10 @@ class ClassificationHandler:
             pipeline,
             X,
             y,
-            cv=CV,
-            scoring=SCORE_METRIC,
+            cv=self.CV,
+            scoring=self.SCORE_METRIC,
             error_score="raise",
-            n_jobs=N_JOBS,
+            n_jobs=self.N_JOBS,
         )
         click.echo(
             f"Scores for {classifier_name} with {variation_description}: {scores}"
@@ -187,7 +143,7 @@ class ClassificationHandler:
 
         if self.calc_cm:
             confusion_matrix = metrics.confusion_matrix(
-                y, cross_val_predict(pipeline, X, y, cv=CV, n_jobs=N_JOBS)
+                y, cross_val_predict(pipeline, X, y, cv=self.CV, n_jobs=self.N_JOBS)
             )
             _report_confusion_matrix(
                 classifier_name,
@@ -197,9 +153,19 @@ class ClassificationHandler:
                 self.output_directory,
             )
 
+    def _convert_to_time_series(self, df: pd.DataFrame) -> pd.Series:
+        df[data.TIME] = pd.to_datetime(df[data.TIME], unit=data.TIME_UNIT)
+        df.set_index(data.TIME, inplace=True)
+        time_series = df[data.PD]
+        return (
+            time_series.asfreq(self.MAX_FREQUENCY, fill_value=0.0)
+            .resample(self.FREQUENCY)
+            .max()
+        )
+
     def do_1d_sequence_classification(self):
         time_serieses = [
-            _convert_to_time_series(df.drop(data.CLASS, axis=1)[1:])
+            self._convert_to_time_series(df.drop(data.CLASS, axis=1))
             for df in self.measurements
         ]
         min_len = min([len(time_series) for time_series in time_serieses])
@@ -208,9 +174,9 @@ class ClassificationHandler:
         )
         y = data.get_defects(self.measurements)
 
-        for classifier_name, classifier in SEQUENCE_CLASSIFIERS.items():
+        for classifier_name, classifier in self.SEQUENCE_CLASSIFIERS:
             self._cross_validate(
-                classifier_name, make_pipeline(classifier), X, y, ONED_TS
+                classifier_name, make_pipeline(classifier), X, y, self.ONED_TS
             )
             _echo_visual_break()
 
@@ -225,25 +191,64 @@ class ClassificationHandler:
         )
         y = data.get_defects(measurements)
 
-        for classifier_name, classifier in SEQUENCE_CLASSIFIERS.items():
+        for classifier_name, classifier in self.SEQUENCE_CLASSIFIERS:
             self._cross_validate(
-                classifier_name, make_pipeline(classifier), X, y, TWOD_TS
+                classifier_name, make_pipeline(classifier), X, y, self.TWOD_TS
             )
             _echo_visual_break()
+
+    def _build_fingerprint_sequence(self, df: pd.DataFrame, finger_algo):
+        timedelta_sum = pd.Timedelta(0)
+        index_sequence_splits = []
+        for index, value in df[data.TIME_DIFF][1:].iteritems():
+            timedelta_sum += pd.Timedelta(value, unit=data.TIME_UNIT)
+            if timedelta_sum >= self.FINGERPRINT_SEQUENCE_DURATION:
+                index_sequence_splits.append(index)
+                timedelta_sum = pd.Timedelta(0)
+        sequence = [df.iloc[: index_sequence_splits[0]]]
+        for idx in range(1, len(index_sequence_splits)):
+            sequence.append(
+                df.iloc[index_sequence_splits[idx - 1] : index_sequence_splits[idx]]
+            )
+
+        too_short_indexes = []
+        for index, sub_df in enumerate(sequence):
+            if len(sub_df.index) <= 2:
+                too_short_indexes.append(index)
+
+        if len(too_short_indexes) > 0:
+            if (
+                len(too_short_indexes) - len(range(too_short_indexes[0], len(sequence)))
+                <= 1
+            ):
+                del sequence[too_short_indexes[0] :]
+            else:
+                raise ValueError(
+                    f"Invalid measurement file: {self.FINGERPRINT_SEQUENCE_DURATION}"
+                )
+
+        assert all([len(sub_df.index) >= 3 for sub_df in sequence])
+        assert all([not sub_df.index.isnull().any() for sub_df in sequence])
+        assert len(sequence) >= 3
+
+        return fingerprint.build_set(sequence, finger_algo, False).to_numpy()
 
     def do_fingerprint_sequence_classification(self):
         measurements = _drop_unneded_columns(self.measurements)
         y = data.get_defects(measurements)
 
         measurements = [df.drop(data.CLASS, axis=1) for df in measurements]
-        for finger_sequence_id, finger_algo in FINGER_SEQUENCES.items():
+        for finger_sequence_id, finger_algo in self.FINGER_SEQUENCES.items():
             X = to_time_series_dataset(
-                [_build_fingerprint_sequence(df, finger_algo) for df in measurements]
+                [
+                    self._build_fingerprint_sequence(df, finger_algo)
+                    for df in measurements
+                ]
             )
             assert not np.isinf(X).any()
             assert np.isnan(X).any()
 
-            for classifier_name, classifier in SEQUENCE_CLASSIFIERS.items():
+            for classifier_name, classifier in self.SEQUENCE_CLASSIFIERS:
                 self._cross_validate(
                     classifier_name, make_pipeline(classifier), X, y, finger_sequence_id
                 )
@@ -252,13 +257,13 @@ class ClassificationHandler:
     def do_fingerprint_classification(self):
         measurements = _drop_unneded_columns(self.measurements)
 
-        for finger_algo_name, finger_algo in FINGERPRINTS.items():
+        for finger_algo_name, finger_algo in self.FINGERPRINTS.items():
             fingerprints = fingerprint.build_set(measurements, finger_algo)
 
             X = fingerprints.drop(data.CLASS, axis=1)
             y = fingerprints[data.CLASS]
 
-            for classifier_name, classifier in FINGERPRINT_CLASSIFIERS.items():
+            for classifier_name, classifier in self.FINGERPRINT_CLASSIFIERS:
                 self._cross_validate(
                     classifier_name,
                     make_pipeline(MinMaxScaler(), classifier),
@@ -268,6 +273,22 @@ class ClassificationHandler:
                     f"fingerprint {finger_algo_name}",
                 )
                 _echo_visual_break()
+
+    def plot_results(self):
+        click.echo(self.mean_accuracies)
+
+        ax = self.mean_accuracies.plot.bar(rot=0, yerr=self.std_accuracies)
+        score_metric_name = self.SCORE_METRIC.replace("_", " ")
+        ax.set_ylabel(score_metric_name)
+        ax.set_title(
+            f"{score_metric_name} by classifier and fingerprint"
+            f" for {self.CV}-fold CV on {len(self.measurements)} files"
+            f" for {len(_get_defect_names(self.measurements))} defects"
+        )
+        ax.legend(loc=3)
+        util.finish_plot(
+            f"classifiers_{self.SCORE_METRIC}_bar", self.output_directory, False
+        )
 
 
 @click.command()
@@ -294,23 +315,7 @@ def main(input_directory, output_directory, calc_cm: bool):
     )
     # FIXME Issue #38
     classificationHandler.do_1d_sequence_classification()
-
     classificationHandler.do_2d_sequence_classification()
-
     classificationHandler.do_fingerprint_sequence_classification()
-
     classificationHandler.do_fingerprint_classification()
-
-    click.echo(classificationHandler.mean_accuracies)
-
-    ax = classificationHandler.mean_accuracies.plot.bar(
-        rot=0, yerr=classificationHandler.std_accuracies
-    )
-    ax.set_ylabel(SCORE_METRIC_NAME)
-    ax.set_title(
-        f"{SCORE_METRIC_NAME} by classifier and fingerprint"
-        f" for {CV}-fold CV on {len(measurements)} files"
-        f" for {len(_get_defect_names(measurements))} defects"
-    )
-    ax.legend(loc=3)
-    util.finish_plot(f"classifiers_{SCORE_METRIC}_bar", output_directory, False)
+    classificationHandler.plot_results()
