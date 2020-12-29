8cd797a7e18882c48f77cc5ec08c62dfb76a89bf
Joris Clement <j.clement@campus.tu-berlin.de>
2020-12-29 15:52:11 +0100

    Improve making data tsfresh compatible

--- src/thesis/tsfresh_features.py
+++ src/thesis/tsfresh_features.py
@@ -1,6 +1,6 @@
 from pathlib import Path
 import sys
-from typing import Union
+from typing import List, Union
 
 import pandas as pd
 from tsfresh import extract_features
@@ -10,22 +10,20 @@ from tsfresh.utilities.dataframe_functions import impute
 from . import data
 
 
-def _drop_unneded_columns(measurements):
-    for measurement in measurements:
-        measurement.drop(
-            columns=[data.TIME_DIFF, data.PD_DIFF, data.VOLTAGE_SIGN, data.CLASS],
-            errors="raise",
-            inplace=True,
-        )
-        measurement.drop(columns=[data.TEST_VOLTAGE], errors="ignore", inplace=True)
+def convert_to_tsfresh_dataset(measurements: List[pd.DataFrame]) -> pd.DataFrame:
+    measurements = [m.loc[:, [data.TIME, data.PD]] for m in measurements]
+    measurements = [_convert_to_time_series(df, i) for i, df in enumerate(measurements)]
+    min_len = min([len(m) for m in measurements])
+    measurements = [df[: min(len(df.index), 10 * min_len)] for df in measurements]
+    return pd.concat(measurements)
 
 
-def _convert_to_time_series(df: pd.DataFrame, frequency) -> pd.Series:
-    MAX_FREQUENCY = pd.tseries.frequencies.to_offset("50us")
-    df[data.TIME] = pd.to_datetime(df[data.TIME], unit=data.TIME_UNIT)
+def _convert_to_time_series(df: pd.DataFrame, index: int) -> pd.DataFrame:
+    df.loc[:, data.TIME] = pd.to_datetime(df[data.TIME], unit=data.TIME_UNIT)
     df.set_index(data.TIME, inplace=True)
-    time_series = df.asfreq(MAX_FREQUENCY, fill_value=0.0).resample(frequency).max()
-    time_series.reset_index(inplace=True)
+    time_series = df.rename(columns={data.PD: "value"})
+    time_series["kind"] = data.PD
+    time_series["id"] = index
     return time_series
 
 
@@ -37,24 +35,16 @@ def main(input_directory: Union[str, Path] = sys.argv[1]):
     measurements, _ = data.read_recursive(input_directory)
     data.clip_neg_pd_values(measurements)
     y = pd.Series(data.get_defects(measurements))
-    _drop_unneded_columns(measurements)
 
-    frequency = pd.tseries.frequencies.to_offset("500ms")
-    measurements = [_convert_to_time_series(df, frequency) for df in measurements]
-    for index, df in enumerate(measurements):
-        df["ID"] = index
-    min_len = min([len(m) for m in measurements])
-    measurements = [df[: min(len(df.index), 8 * min_len)] for df in measurements]
-    all_df = pd.concat(measurements)
-    all_df = all_df.astype({"ID": "ushort"})
-    print(all_df)
+    all_df = convert_to_tsfresh_dataset(measurements)
 
     extracted_features = extract_features(
         all_df,
-        column_id="ID",
-        column_sort=data.TIME,
-        show_warnings=True,
+        column_id="id",
+        column_kind="kind",
+        column_value="value",
         impute_function=impute,
+        show_warnings=True,
     )
     print("extracted_features.shape: ", extracted_features.shape)
     relevance_table = calculate_relevance_table(
