2333e96fbf3a5c76260695a5124a60e081dcd69b
Joris Clement <j.clement@campus.tu-berlin.de>
2021-01-05 22:14:03 +0100

    Fix test & simplify script usage

--- tests/test_tsfresh_features.py
+++ tests/test_tsfresh_features.py
@@ -1,13 +1,26 @@
+import click.testing
 import pandas as pd
 
 from thesis import tsfresh_features
 
 
 def test_main(csv_folder):
-    relevance_table = tsfresh_features.main(csv_folder)
+    relevance_table = tsfresh_features.main(csv_folder, n_jobs=1)
     assert type(relevance_table) is pd.DataFrame
 
 
 def test_convert_to_tsfresh_dataset(measurements):
     dataset = tsfresh_features.convert_to_tsfresh_dataset(measurements)
     assert type(dataset) is pd.DataFrame
+
+
+def test_main_version_succeeds():
+    runner = click.testing.CliRunner()
+    result = runner.invoke(tsfresh_features.main, ["--version"])
+    assert result.exit_code == 0
+
+
+def test_main_help_succeeds():
+    runner = click.testing.CliRunner()
+    result = runner.invoke(tsfresh_features.main, ["--help"])
+    assert result.exit_code == 0
