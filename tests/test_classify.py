4f13b07636a8f475573b8dd1fb3b01bd53399b8e
Joris Clement <j.clement@campus.tu-berlin.de>
2021-01-06 12:08:18 +0100

    Create output dir & complain if it doesn't exist

--- tests/test_classify.py
+++ tests/test_classify.py
@@ -24,7 +24,7 @@ def multiple_csv_files(csv_folder, tmpdir):
 @pytest.fixture
 def config_and_multiple_csv_files(config, csv_folder, tmpdir):
     config["general"]["data_dir"] = str(tmpdir)
-    config["general"]["output_dir"] = str(tmpdir)
+    config["general"]["output_dir"] = str(Path(tmpdir, "output"))
     config_filepath = Path(tmpdir, "config.yml")
     with open(config_filepath, "w") as outfile:
         yaml.dump(config, outfile)
@@ -46,7 +46,7 @@ def test_classify_main_calc_confusion_matrix_succeeds(
     config, multiple_csv_files, tmpdir
 ):
     config["general"]["data_dir"] = str(multiple_csv_files)
-    config["general"]["output_dir"] = str(tmpdir)
+    config["general"]["output_dir"] = str(Path(tmpdir, "output"))
     config["general"]["calc_cm"] = True
 
     handler = classify.ClassificationHandler(config)
