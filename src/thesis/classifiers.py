6c419ef0d8449f6a3694e461e844ecfa7b5ed9c8
Joris Clement <j.clement@campus.tu-berlin.de>
2021-01-08 10:46:27 +0100

    Fix classifier: Add predict_proba functionality
    This is needed for the metric top_k_accuracy_score

--- src/thesis/classifiers.py
+++ src/thesis/classifiers.py
@@ -1,8 +1,7 @@
 import pandas as pd
-from sklearn.base import BaseEstimator, ClassifierMixin
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.utils.multiclass import unique_labels
-from sklearn.utils.validation import check_array, check_is_fitted, check_X_y
+from sklearn.utils.validation import check_X_y
 
 
 def _calc_mean_per_defect(x_train, y_train, defects):
@@ -18,28 +17,17 @@ def _calc_mean_per_defect(x_train, y_train, defects):
     return mean_per_defect
 
 
-class LukasMeanDist(ClassifierMixin, BaseEstimator):
+class LukasMeanDist(KNeighborsClassifier):
+    def __init__(self):
+        super().__init__(n_neighbors=1)
+
     def fit(self, X, y):
         X, y = check_X_y(X, y)
-        self.classes_ = list(unique_labels(y))
-        mean_per_defect = _calc_mean_per_defect(X, y, self.classes_)
+        mean_per_defect = _calc_mean_per_defect(X, y, list(unique_labels(y)))
         y_mean = list(mean_per_defect.keys())
-        assert y_mean == self.classes_
         X_mean = pd.DataFrame(mean_per_defect.values())
-        self.k_nn_ = KNeighborsClassifier(n_neighbors=1).fit(X_mean, y_mean)
-        return self
-
-    def predict(self, X):
-        check_is_fitted(self)
-        X = check_array(X)
-
-        return self.k_nn_.predict(X)
-
-    def get_params(self, deep=True):
-        return dict()
-
-    def set_params(self, **parameters):
+        super().fit(X_mean, y_mean)
         return self
 
     def _more_tags(self):
-        return {"X_types": ["2darray", "1dlabels"]}
+        return {"multioutput": False}
