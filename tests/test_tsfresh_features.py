545cd48cd1b54c7c483fc2d41f4f6ed3b1772c76
Joris Clement <j.clement@campus.tu-berlin.de>
2021-01-11 10:56:27 +0100

    Add option to save extracted features to csv
    This is the first step to precompute the tsfresh features.
    1. Save extracted features
    2. Save selected features

--- tests/test_tsfresh_features.py
+++ tests/test_tsfresh_features.py
@@ -1,12 +1,21 @@
+from pathlib import Path
+
 import click.testing
 import pandas as pd
 
 from thesis import tsfresh_features
 
 
-def test_main(csv_folder):
+def test_calc_relevant_features(csv_folder):
     relevance_table = tsfresh_features.calc_relevant_features(csv_folder, n_jobs=1)
     assert type(relevance_table) is pd.DataFrame
+
+
+def test_extracted_features_saved(csv_folder, tmpdir):
+    output_file = Path(tmpdir, "extracted_features.csv")
+    assert not output_file.exists()
+    tsfresh_features.calc_relevant_features(csv_folder, 1, output_file)
+    assert output_file.exists()
 
 
 def test_convert_to_tsfresh_dataset(measurements):
