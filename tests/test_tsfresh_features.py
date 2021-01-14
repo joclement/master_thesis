e5ab8852a12ee4ef4e2257b739991f79abe82d38
Joris Clement <j.clement@campus.tu-berlin.de>
2021-01-14 10:08:36 +0100

    Cleanup

--- tests/test_tsfresh_features.py
+++ tests/test_tsfresh_features.py
@@ -1,7 +1,6 @@
 from pathlib import Path
 
 import click.testing
-import pandas as pd
 
 from thesis import tsfresh_features
 
@@ -10,7 +9,9 @@ def test_main_with_features_saved(csv_folder, tmpdir):
     output_file = Path(tmpdir, "extracted_features.data")
     assert not output_file.exists()
     runner = click.testing.CliRunner()
-    result = runner.invoke(tsfresh_features.main, [csv_folder, "-j", 1, "-o", str(output_file)])
+    result = runner.invoke(
+        tsfresh_features.main, [csv_folder, "-j", 1, "-o", str(output_file)]
+    )
     assert result.exit_code == 0
     assert output_file.exists()
 
