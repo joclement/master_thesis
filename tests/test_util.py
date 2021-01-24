0f34183ca3c00fabe33e2c9cba5c18576b25be90
Joris Clement <j.clement@campus.tu-berlin.de>
2021-01-24 21:08:32 +0100

    Add check against testdata to test

--- tests/test_util.py
+++ tests/test_util.py
@@ -1,11 +1,14 @@
 import pandas as pd
+from pytest import approx
 
 from thesis import data, util
 
 
-def test_to_dataTime():
+def test_to_dataTime(measurement):
     assert data.TIME_UNIT == "ms"
 
     duration = pd.Timedelta("30 seconds")
     assert util.to_dataTIME(duration) == 30000
     assert type(util.to_dataTIME(duration)) is int
+
+    assert measurement[data.TIME_DIFF].sum() == approx(129200)
