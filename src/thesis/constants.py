607a5660f721f68c50d6da3d20aefec6908261e5
Joris Clement <j.clement@campus.tu-berlin.de>
2021-01-29 18:11:56 +0100

    Add typing information

--- src/thesis/constants.py
+++ src/thesis/constants.py
@@ -4,10 +4,10 @@ SCORES_FILENAME: Final = "models_scores.csv"
 CONFIG_FILENAME: Final = "config.yml"
 CONFIG_MODELS_RUN_ID: Final = "models-to-run"
 
-TRAIN_ACCURACY = "train_accuracy"
-TRAIN_SCORE = "train"
-VAL_SCORE = "val"
-ACCURACY_SCORE = "accuracy"
-TOP_K_ACCURACY = "top_3_accuracy"
-FILE_SCORE = "file_score"
-METRIC_NAMES = {ACCURACY_SCORE, TOP_K_ACCURACY, TRAIN_ACCURACY}
+TRAIN_ACCURACY: Final = "train_accuracy"
+TRAIN_SCORE: Final = "train"
+VAL_SCORE: Final = "val"
+ACCURACY_SCORE: Final = "accuracy"
+TOP_K_ACCURACY: Final = "top_3_accuracy"
+FILE_SCORE: Final = "file_score"
+METRIC_NAMES: Final = {ACCURACY_SCORE, TOP_K_ACCURACY, TRAIN_ACCURACY}
