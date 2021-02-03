c6a53a2ff7863aed2618b079541c3ff0c70c2884
Joris Clement <j.clement@campus.tu-berlin.de>
2021-02-03 21:35:18 +0100

    Put model complete in pipeline
    As caching is then longer possible, as I have done it, let joblib and
    the pipeline do the caching.
    
    Towards #59.

--- tests/test_models.py
+++ tests/test_models.py
@@ -1,12 +1,12 @@
-import numpy as np
 import pandas as pd
 import pytest
 
 from thesis import data, models
 
 
-def test_knn_dtw_seqfinger_seqown_model(measurements):
-    models_config = {
+@pytest.fixture
+def models_config():
+    return {
         "knn_dtw-seqfinger_seqown": {
             "normalize": True,
             "data": {
@@ -15,13 +15,13 @@ def test_knn_dtw_seqfinger_seqown_model(measurements):
             },
         }
     }
+
+
+def test_knn_dtw_seqfinger_seqown_model(models_config, measurements):
     y = pd.Series(data.get_defects(measurements))
 
-    modelHandler = models.ModelHandler(measurements, y, models_config, False, None)
-    pipeline, X = modelHandler.get_model_with_data("knn_dtw-seqfinger_seqown")
+    modelHandler = models.ModelHandler(y, models_config, False, None)
+    pipeline = modelHandler.get_model("knn_dtw-seqfinger_seqown")
 
-    scaler = list(pipeline.named_steps.values())[0]
-    X_transformed = scaler.fit_transform(X)
-    assert X.shape == X_transformed.shape
-    assert np.nanmax(X_transformed) == pytest.approx(1.0)
-    assert np.nanmin(X_transformed) == 0.0
+    assert len(pipeline.steps) == 3
+    pipeline.fit(measurements, y)
