b1ee284f89ec0dc18648a5865b08dde5ecb8fe9e
Joris Clement <j.clement@campus.tu-berlin.de>
2021-02-04 22:15:31 +0100

    Add debug estimator

--- src/thesis/util.py
+++ src/thesis/util.py
@@ -1,5 +1,6 @@
 import matplotlib.pyplot as plt
 import pandas as pd
+from sklearn.base import BaseEstimator, TransformerMixin
 
 
 def finish_plot(name: str, output_folder, show: bool = False):
@@ -12,3 +13,13 @@ def finish_plot(name: str, output_folder, show: bool = False):
 
 def to_dataTIME(time: pd.Timedelta) -> int:
     return int(time.value / 10 ** 6)
+
+
+class Debug(BaseEstimator, TransformerMixin):
+    def transform(self, X):
+        print(pd.DataFrame(X).head())
+        print(X.shape)
+        return X
+
+    def fit(self, X, y=None, **fit_params):
+        return self
