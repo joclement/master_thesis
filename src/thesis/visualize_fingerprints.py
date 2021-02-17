5252c3081e070e59b511e2f1fd6116668a07f5c8
Joris Clement <j.clement@campus.tu-berlin.de>
2021-02-17 17:46:54 +0100

    Split data also for visualizing fingerprints

--- src/thesis/visualize_fingerprints.py
+++ src/thesis/visualize_fingerprints.py
@@ -5,7 +5,7 @@ import pandas as pd
 from scipy.cluster import hierarchy
 import seaborn as sns
 
-from . import __version__, data, fingerprint, util
+from . import __version__, data, fingerprint, prepared_data, util
 
 
 def _generate_heatmap(fingerprints: pd.DataFrame, output_folder, show):
@@ -19,7 +19,7 @@ def _generate_pairplots(fingerprints: pd.DataFrame, output_folder, show):
         parameters = fingerprint.get_parameter_group(fingerprints, group)
         parameters[data.CLASS] = data.get_names(fingerprints[data.CLASS])
 
-        pairgrid = sns.pairplot(parameters, hue=data.CLASS, diag_kind="hist")
+        pairgrid = sns.pairplot(parameters, hue=data.CLASS)
         pairgrid.fig.suptitle(
             f"Pairwise relationships in fingerprint parameters related to {group}",
             y=1.08,
@@ -46,9 +46,12 @@ def _generate_dendogram(measurements: List[pd.DataFrame], output_folder, show):
     help="Folder to save figures in",
 )
 @click.option("--show", "-s", is_flag=True, help="Show plots")
-def main(path, output_folder, show):
+@click.option("--split", "-s", is_flag=True, help="Split data into 60 seconds samples")
+def main(path, output_folder, show, split):
     "Plot visualization of measurement file csv"
     measurements, _ = data.read_recursive(path)
+    if split:
+        measurements = prepared_data.adapt_durations(measurements)
 
     fingerprints = fingerprint.build_set(measurements, fingerprint.ott, add_class=True)
     _generate_heatmap(fingerprints, output_folder, show)
