047203907b0dde372cd6d020566d3f98627be340
Joris Clement <j.clement@campus.tu-berlin.de>
2021-03-04 13:58:32 +0100

    Add plots to visualize grid results

--- src/thesis/visualize_grid.py
+++ src/thesis/visualize_grid.py
@@ -1,8 +1,12 @@
 import pickle
 
 import click
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import seaborn as sns
 
-from . import __version__
+from . import __version__, util
 
 
 @click.command()
@@ -12,3 +16,32 @@ def main(grid_file):
     with open(grid_file, "rb") as f:
         grid_results = pickle.load(f)
     print(grid_results)
+
+    _, ax = plt.subplots()
+    y_pos = np.arange(len(grid_results["mean_test_score"]))
+    ax.barh(
+        y_pos,
+        grid_results["mean_test_score"],
+        xerr=grid_results["std_test_score"],
+        align="center",
+    )
+    ax.set_yticks(y_pos)
+    ax.set_xlabel("file-based Balanced accuracy")
+    ax.set_xlim([0, 1])
+    util.finish_plot(f"mean-grid-score", "./", True)
+
+    sns.swarmplot(data=combine_split_scores(grid_results), x="score", y="combination")
+    util.finish_plot(f"mean-grid-score", "./", True)
+
+
+def combine_split_scores(grid_results):
+    return pd.DataFrame(
+        [
+            {
+                "score": grid_results[f"split{split_idx}_test_score"][i],
+                "combination": f"c{i}",
+            }
+            for i in range(len(grid_results["mean_test_score"]))
+            for split_idx in range(4)
+        ]
+    )
