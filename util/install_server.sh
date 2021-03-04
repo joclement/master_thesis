e2b010c7dc237f09ecb7aad00a8343df9a6c5ee1
Joris Clement <j.clement@campus.tu-berlin.de>
2021-03-04 11:07:18 +0100

    Add script to install project for calculations
    I used this to install this project on a server for computations.

---
+++ util/install_server.sh
@@ -0,0 +1,19 @@
+sudo apt update
+sudo apt upgrade -y
+sudo apt install -y make build-essential libssl-dev zlib1g-dev libbz2-dev libreadline-dev libsqlite3-dev wget curl llvm libncurses5-dev libncursesw5-dev xz-utils tk-dev libffi-dev liblzma-dev python-openssl git
+
+git clone https://github.com/pyenv/pyenv.git ~/.pyenv
+echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.bashrc
+echo 'export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.bashrc
+echo -e 'if command -v pyenv 1>/dev/null 2>&1; then\n eval "$(pyenv init -)"\nfi' >> ~/.bashrc
+exec "$SHELL"
+
+pyenv install 3.8.7
+pyenv local 3.8.7
+curl -sSL https://raw.githubusercontent.com/python-poetry/poetry/master/get-poetry.py | python -
+source $HOME/.poetry/env
+
+git clone https://github.com/flyingdutchman23/thesis_implementation.git
+cd thesis_implementation
+pyenv local 3.8.7
+mkdir -p data/csv-files/prepared/dataset
