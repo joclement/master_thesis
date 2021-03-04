5ce062be6ab603a0103e641d1f0d68218edf7b0f
Joris Clement <j.clement@campus.tu-berlin.de>
2021-03-04 23:13:48 +0100

    Improve grid visualizing script

--- src/thesis/visualize_grid.py
+++ src/thesis/visualize_grid.py
@@ -17,21 +17,47 @@ def main(grid_file):
         grid_results = pickle.load(f)
     print(grid_results)
 
+    for i, rank in enumerate(grid_results["rank_test_score"]):
+        if rank <= 10:
+            print(f"Rank {rank}:")
+            print(f"{grid_results['params'][i]}")
     _, ax = plt.subplots()
     y_pos = np.arange(len(grid_results["mean_test_score"]))
     ax.barh(
         y_pos,
         grid_results["mean_test_score"],
+        tick_label=grid_results["rank_test_score"],
         xerr=grid_results["std_test_score"],
         align="center",
     )
     ax.set_yticks(y_pos)
     ax.set_xlabel("file-based Balanced accuracy")
+    ax.set_xlabel("file-based Balanced accuracy")
     ax.set_xlim([0, 1])
-    util.finish_plot("mean-grid-score", "./", True)
+    util.finish_plot("mean-grid-score", None, True)
 
-    sns.swarmplot(data=combine_split_scores(grid_results), x="score", y="combination")
-    util.finish_plot("mean-grid-score", "./", True)
+    scores = combine_split_scores(grid_results)
+    scores = drop_unneeded_classifiers(scores)
+    sns.swarmplot(data=scores, x="score", y="combination", hue="run")
+    util.finish_plot("mean-grid-score", None, True)
+
+
+def drop_unneeded_classifiers(scores):
+    # scores = scores.loc[scores["classifier"] != "mlp", :]
+    # scores = scores.loc[scores["classifier"] != "RandomForestClassifier()", :]
+    # scores = scores.loc[scores["classifier"] != "KNeighborsClassifier()", :]
+    # scores = scores.loc[scores["classifier"] != "SVC()", :]
+    return scores
+
+
+def get_classifier_name(params):
+    if "classifier" not in params:
+        return "mlp"
+    return str(params["classifier"])
+
+
+def get_params_description(params):
+    return str(list(params.values())[1:])
 
 
 def combine_split_scores(grid_results):
@@ -39,9 +65,12 @@ def combine_split_scores(grid_results):
         [
             {
                 "score": grid_results[f"split{split_idx}_test_score"][i],
-                "combination": f"c{i}",
+                "classifier": get_classifier_name(grid_results["params"][i]),
+                "combination": get_params_description(grid_results["params"][i]),
+                "run": split_idx,
             }
             for i in range(len(grid_results["mean_test_score"]))
             for split_idx in range(4)
+            if grid_results["rank_test_score"][i] <= 10
         ]
     )
