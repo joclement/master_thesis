aa6a4d8355bf7b2378fd04aa949c8c08d36324ce
Joris Clement <j.clement@campus.tu-berlin.de>
2021-03-08 12:18:26 +0100

    Improve visualize grid plot, #64

--- src/thesis/visualize_grid.py
+++ src/thesis/visualize_grid.py
@@ -41,6 +41,19 @@ def main(grid_file):
     sns.swarmplot(data=scores, x="score", y="combination", hue="run")
     util.finish_plot("mean-grid-score", None, True)
 
+    skip = "classifier__min_child_samples"
+    scores = combine_split_scores_relplot(grid_results, skip)
+    print(scores)
+    sns.relplot(
+        data=scores,
+        x=skip,
+        y="score",
+        col="combination",
+        col_wrap=4,
+        kind="line",
+    )
+    util.finish_plot("relplot-score", None, True)
+
 
 def drop_unneeded_classifiers(scores):
     # scores = scores.loc[scores["classifier"] != "mlp", :]
@@ -56,21 +69,45 @@ def get_classifier_name(params):
     return str(params["classifier"])
 
 
-def get_params_description(params):
-    return str(list(params.values())[1:])
+def get_params_description(params, skip):
+    return ",".join(str(v) for k, v in params.items() if k != skip)
+
+
+def build_row(grid_results, i, split_idx):
+    row = {
+        "score": grid_results[f"split{split_idx}_test_score"][i],
+        "combination": get_params_description(grid_results["params"][i], ""),
+        "run": split_idx,
+        "i": i,
+    }
+    return row
 
 
 def combine_split_scores(grid_results):
     return pd.DataFrame(
         [
-            {
-                "score": grid_results[f"split{split_idx}_test_score"][i],
-                "classifier": get_classifier_name(grid_results["params"][i]),
-                "combination": get_params_description(grid_results["params"][i]),
-                "run": split_idx,
-            }
+            build_row(grid_results, i, split_idx)
             for i in range(len(grid_results["mean_test_score"]))
             for split_idx in range(4)
             if grid_results["rank_test_score"][i] <= 10
+        ]
+    )
+
+
+def build_row_relplot(grid_results, i, skip):
+    row = {
+        "score": grid_results["mean_test_score"][i],
+        "combination": get_params_description(grid_results["params"][i], skip),
+        "i": i,
+    }
+    row.update(grid_results["params"][i])
+    return row
+
+
+def combine_split_scores_relplot(grid_results, skip):
+    return pd.DataFrame(
+        [
+            build_row_relplot(grid_results, i, skip)
+            for i in range(len(grid_results["mean_test_score"]))
         ]
     )
