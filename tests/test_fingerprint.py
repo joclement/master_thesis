c9ca1b540d5f296c0c0a19f363e40cd7a5a8ed0a
Joris Clement <j.clement@campus.tu-berlin.de>
2021-04-22 19:02:06 +0200

    Improve relown feature set

--- tests/test_fingerprint.py
+++ tests/test_fingerprint.py
@@ -57,17 +57,17 @@ def test_normalize_fingerprints(measurements):
 
 def test_fingerprint_relown_set(measurements):
     dataset = fingerprint.build_set(measurements, fingerprint=fingerprint.relown)
-    assert dataset.shape == (6, 35)
+    assert dataset.shape == (6, 34)
 
 
 def test_fingerprint_own_set(measurements):
     dataset = fingerprint.build_set(measurements, fingerprint=fingerprint.own)
-    assert dataset.shape == (6, 40)
+    assert dataset.shape == (6, 39)
 
 
 def test_fingerprint_own(measurement):
     finger = fingerprint.own(measurement)
-    assert len(finger) == 40
+    assert len(finger) == 39
 
 
 @fixture
