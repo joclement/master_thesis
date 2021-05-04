6487ae20c73491caa45e5fc8043edbf2359ea369
Joris Clement <j.clement@campus.tu-berlin.de>
2021-05-04 22:11:56 +0200

    Avoid warning by LGBM library

--- src/thesis/grid_search.py
+++ src/thesis/grid_search.py
@@ -62,7 +62,7 @@ FINGERPRINT_COMPARE_GRID = [
     {
         "scaler": ["passthrough"],
         "classifier": [LGBMClassifier(random_state=RANDOM_STATE)],
-        "classifier__feature_fraction": [1.0, 0.8],
+        "classifier__colsample_bytree": [1.0, 0.8],
         "classifier__min_split_gain": [0.0, 0.01, 0.1, 0.2],
         "classifier__num_leaves": [10, 30, 50],
         "classifier__max_bin": [63, 127, 255],
