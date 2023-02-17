84f2062d4d0a3c5fcc885923a53c300d9f80edd3
Joris Clement <7713214+joclement@users.noreply.github.com>
2023-02-17 11:55:05 +0100

    Have nox install correct versions

--- noxfile.py
+++ noxfile.py
@@ -1,13 +1,12 @@
 import nox
-import nox_poetry
+from nox_poetry import session
 
 nox.options.sessions = "lint", "mypy", "tests"
 
 LOCATIONS = "src", "tests", "noxfile.py"
 
 
-@nox.session(python="3.8")
-@nox_poetry.session
+@session(python="3.8")
 def tests(session):
     args = session.posargs or [
         "tests",
@@ -22,20 +21,19 @@ def tests(session):
     session.run("pytest", *args)
 
 
-@nox.session(python="3.8")
+@session(python="3.8")
 def lint(session):
     session.install("flake8", "flake8-black", "flake8-import-order")
     session.run("flake8", *LOCATIONS)
 
 
-@nox.session(python="3.8")
+@session(python="3.8")
 def black(session):
     session.install("black")
     session.run("black", *LOCATIONS)
 
 
-@nox.session(python="3.8")
-@nox_poetry.session
+@session(python="3.8")
 def mypy(session):
     session.install("mypy")
     session.run("mypy", *LOCATIONS)
